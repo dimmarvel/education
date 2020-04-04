745f6c4f40691e32ac9db4b42b2a48762c1edf8c
Dima <dimmarvel@mail.ru>
2020-04-04 19:42:51 +0300

    Ex 1 2, 1 3, 1 4

--- C/The C Programming Language/Ex 1 1.c
+++ C/The C Programming Language/Ex 1 1.c
@@ -1,5 +1,45 @@
 #include<stdio.h>
 
 main() {
-	printf("Hello, world.");
+	//Ex 1 1
+	printf("Hello, world.\n");
+	printf("--------------------------\n");
+	//Ex 1 2
+	printf("\aHello\n");
+
+	//Ex 1 3
+	printf("--------------------------\n");
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 50;
+
+	fahr = lower;
+
+	printf("\tFarengate to Celsius\n");
+	while (fahr <= upper) {
+		celsius = (5.0 / 9.0)*(fahr - 32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+
+	//Ex 1 4
+	printf("--------------------------\n");
+
+	lower = 0;
+	upper = 300;
+	step = 50;
+
+	celsius = lower;
+
+	printf("\tCelsius to Farengate\n");
+	while (celsius <= upper) {
+		fahr = celsius*(5.0/9.0)+32;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+
+	system("pause");
 }
