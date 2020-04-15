95b00be90a8d2110b78c8aac51fce744419c1d0b
Dima <dimmarvel@mail.ru>
2020-04-15 19:35:04 +0300

    STL function

---
+++ С++/STL-std/Function.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<functional>
+#include<vector>
+using namespace std;
+//-------------------------
+void chet(int a) {
+	if ((a % 2) == 0) cout << a << endl;
+}
+
+void NEchet(int a) {
+	if ((a % 2) != 0) cout << a << endl;
+}
+
+void cOUT(vector<int> arr, function<void(int)> fun) {
+	for (int i = 0; i < arr.size(); i++)
+	{
+		fun(arr[i]);
+	}
+}
+//-------------------------
+void foo() {
+	cout << "Im is foo!" << endl;
+}
+void bar() {
+	cout << "Im is bar!" << endl;
+}
+//-------------------------
+int sum(int a, int b) {
+	return a + b;
+}
+//-------------------------
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+	//-------------------------
+	function<void()> f; //создаем переменную типа function
+	// которая принимает функцию с типом возвр. значения void и принимающей 0 аргуменртов
+
+	
+	f = foo;
+
+	f();
+
+	f = bar;
+
+	f();
+	
+	//-------------------------
+	
+	function<int(int,int)> f2;
+	f2 = sum;
+	int res = f2(4, 1);
+	cout << "res - " << res << endl;
+	
+	//-------------------------
+
+	vector<int> arr = { 10,11,12,13,14,15,16,17,18 };
+	function<void(int)> f3;
+	f3 = NEchet;
+	cOUT(arr, f3);
+	cout << endl;
+	f3 = chet;
+	cOUT(arr, f3);
+
+
+	system("Pause");
+}
---
+++ С++/Threads/Thread 5.cpp
@@ -0,0 +1,35 @@
+#include <thread>
+#include <iostream>
+#include <chrono>
+
+using namespace std;
+
+void addfunc(int a, int b) {
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "----------THREAD ADDFUNC START-----------" << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "Sum a + b = " << a + b << endl;
+	this_thread::sleep_for(chrono::milliseconds(3000));
+	cout << "----------THREAD ADDFUNC END-----------" << endl;
+}
+
+
+int main()
+{
+
+	thread th(addfunc,2,3);
+
+	
+	for (int i = 0; true ; i++)
+	{
+		cout << "ID - " << this_thread::get_id() << "\t MAIN IS WORK\t - " << i << endl;
+		
+		this_thread::sleep_for(chrono::milliseconds(800));
+
+		//if ((i % 5) == 0)
+		//	addfunc(2, 5);
+	}
+	th.join();
+	system("pause");
+	return 0;
+}
