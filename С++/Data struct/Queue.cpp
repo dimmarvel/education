35ee9bdeb7a60403e99c4af6b4602fc08375c562
Dima <dimmarvel@mail.ru>
2020-04-25 23:08:18 +0300

    Queue

--- С++/Data struct/Queue.cpp
+++ С++/Data struct/Queue.cpp
@@ -1,66 +1,94 @@
 #include <iostream>
+#include <list>
 #include <memory>
+#include <iterator>
 
 using namespace std;
 
 template<typename T>
-class Node {
-	T data;
-	shared_ptr<Node> next;
-};
-
-template<typename T>
 class DQueue {
-	shared_ptr<Node<T>> first;
-	shared_ptr<Node<T>> last;
+	
 	int size;
+	list<T> lst;
+
 public:
-	DQueue();				//create queue |	data = NULL	\ next = NULL
-	DQueue(T t);			//create queue |	data = t	\ next = new T()
-	DQueue(T t, int size);	//create queue |	data = 0	\ next = new T
-	~DQueue();				\
-	void push();
-	void pop();
-	void create();
-	void OutQueue();
+	
+	DQueue();					
+	explicit DQueue(T _val, int size = 1);
+
+	~DQueue();	
+
+	void push_back(T _val);
+	void push_front(T _val);
+	void pop_back();
+	void pop_front();
+	void Out();
+
 };
 
 template<typename T>
-DQueue<T>::DQueue() {
-	if(first)
-	node.data = 0;
-}
+DQueue<T>::DQueue() : lst(lst.push_back(0)) {}
 
 template<typename T>
-DQueue<T>::DQueue(T t) {
-	node.next = new T();
-	node.data = t;
-}
-
-template<typename T>
-DQueue<T>::DQueue(T t, int size) {
+DQueue<T>::DQueue(T _val, int size) {
+	
 	for (int i = 0; i < size; i++)
-	{
-		node.next = new T();
-		node.data = 0;
-	}
+		lst.push_back(_val);
+
+	this->size += size;
 }
 
 template<typename T>
 DQueue<T>::~DQueue() {
-	for (size_t i = 0; i < size; i++)
-	{
-		las
-	}
+	for (int i = 0; i < size; i++)
+		lst.pop_back();
+	size = 0;
 }
 
 template<typename T>
-DQueue<T>::OutQueue() {
+void DQueue<T>::Out() {
 
+	for (auto iter = lst.begin(); iter != lst.end(); iter++)
+		cout << *iter << " ";
+	cout << endl;
+}
+
+template<typename T>
+void DQueue<T>::pop_back() {
+	cout << "Pop_front( " << lst.end() << " size(" << --size << ")" << endl;
+	lst.pop_back();
+}
+
+template<typename T>
+void DQueue<T>::pop_front() {
+	cout << "Pop_front( " << lst.front() << " size(" << --size << ")" << endl;
+	lst.pop_back();
+}
+
+template<typename T>
+void DQueue<T>::push_back(T _val) {
+	size++;
+	lst.push_back(_val);
+	cout << "Push_back( " << _val << " size(" << size << ")" << endl;
+}
+
+template<typename T>
+void DQueue<T>::push_front(T _val) {
+	size++;
+	lst.push_front(_val);
+	cout << "Push_front( " << _val << " size(" << size << ")" << endl;
 }
 
 int main() {
-	DQueue<int> a();
+	DQueue<int> a(10,5);
+	a.Out();
+
+	a.pop_back();
+	a.pop_front();
+	a.push_front(9);
+	a.push_back(8);
+
+	a.Out();
 
 	system("pause");
 }
