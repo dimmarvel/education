c959410543b6dcf7688324e66a0c60e8bfd03452
Dima <dimmarvel@mail.ru>
2020-04-27 16:29:53 +0300

    Adapters container(priority queue, queue)

---
+++ С++/STL-std/Adapters container(priority queue, queue).cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+#include<queue>
+#include<stack>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	queue<int,list<int>> ql;
+	queue<int,deque<int>> qd;
+	queue<int,vector<int>> qv;
+	queue<int,stack<int>> qs;
+	
+
+	priority_queue<int> pq;
+
+	pq.push(1);
+	pq.push(2);
+	pq.push(3);
+	pq.push(10);
+	pq.push(4);
+	pq.push(9);
+
+	while (!pq.empty()) {
+		cout << pq.top() << endl;
+		pq.pop();
+		cout << "size - " << pq.size() << endl;
+	}
+
+	system("Pause");
+}
