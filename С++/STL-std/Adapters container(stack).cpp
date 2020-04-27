6efff2697fd20817f759e90e9c2e7eafcbf3be42
Dima <dimmarvel@mail.ru>
2020-04-27 16:04:42 +0300

    Adapters container(stack)

---
+++ С++/STL-std/Adapters container(stack).cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+#include<stack>
+using namespace std;
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+
+	stack<int,vector<int>> st;
+
+	st.push(2);
+	st.push(32);
+	st.push(4);
+	st.push(102);
+	
+	st.emplace(10);
+
+	cout << "size = " << st.size() << endl;
+
+	st.pop();
+
+	cout << "top - " << st.top() << endl;
+
+	auto a = st._Get_container();
+
+	cout << "a[0] - " << a[0] << endl;
+
+	while (!st.empty()) {
+		cout << st.top() << endl;
+		st.pop();
+	}
+
+	system("Pause");
+}
