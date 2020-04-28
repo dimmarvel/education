b9b9eb4953391e4cc35258142c05c5170f12846e
Dima <dimmarvel@mail.ru>
2020-04-28 10:03:45 +0300

    Algorithm(accumulate)

---
+++ С++/STL-std/Algorithm(accumulate).cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<numeric>
+using namespace std;
+class Person {
+public:
+	Person(string name, double score) {
+		this->name = name;
+		this->score = score;
+	}
+	bool operator()(const Person &p) {
+		return p.score > 100;
+	}
+	string name;
+	double score;
+};
+
+int main() {
+
+	setlocale(LC_ALL, "Rus");
+	vector<int> v = { 1,2,4,2,3,1,7,4 };
+
+	auto it = accumulate(v.begin(), v.end(), 0);
+	auto it2 = accumulate(begin(v), end(v), 0);
+	auto it3 = accumulate(begin(v), end(v), 1, [](int a, int b) {
+		return a * b;
+	});
+	auto it4 = accumulate(begin(v), end(v), 1, [](int a, int b) {
+		return (b % 2 == 0) ? a * b : a;
+	});
+	string it5= accumulate(next(begin(v)), end(v),to_string(v[0]), [](string a, int b) {
+		return a + "-" + to_string(b);
+	});
+
+	cout << "it(accum) - " << it << endl;
+	cout << "it2(accum) - " << it2 << endl;
+	cout << "it3(accum) - " << it3 << endl;
+	cout << "it4(accum) - " << it4 << endl;
+	cout << "it4(accum) - " << it5 << endl;
+
+
+	system("Pause");
+}
