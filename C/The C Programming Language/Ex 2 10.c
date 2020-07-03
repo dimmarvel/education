1b872d8ad8491efd072d1b8be4a006c5d5da9f50
Dmitry <dimmarvel@mail.ru>
2020-07-03 22:06:56 +0300

    Ex 2 10

---
+++ C/The C Programming Language/Ex 2 10.c
@@ -0,0 +1,16 @@
+#include<limits.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int lower(int c)
+{
+	return ((c >= 'A' && c <= 'Z') ? ('a' + (c - 'A')) : c);
+}
+	
+main() 
+{
+	printf("%c\n",(char)lower('C'));
+	printf("%c\n",(char)lower('X'));
+	system("pause");
+}
