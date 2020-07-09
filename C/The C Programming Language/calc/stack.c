127937aa0b83cf73a791e804d8778521031fdb08
Dmitry <dimmarvel@mail.ru>
2020-07-09 08:47:08 +0300

    Ex 4 11

---
+++ C/The C Programming Language/calc/stack.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "calc.h"
+
+#define MAXVAL 100 //maximum depth of val stack
+
+int sp;
+double val[MAXVAL]; //value stack
+int sp = 0; //next free stack position
+
+void push(double f)
+{
+	if (sp < MAXVAL)
+		val[sp++] = f;
+	else
+		printf("error: stack full, can`t push %g\n", f);
+}
+
+double pop(void)
+{
+	if (sp > 0)
+	{
+		return val[--sp];
+	}
+	else
+	{
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
