714b45a5f0bc88a567b28c6add9a8d0a381bf400
Dmitry <dimmarvel@mail.ru>
2020-07-14 20:37:02 +0300

    Ex 5 2

---
+++ C/The C Programming Language/Ex 5 2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int getfloat(float *pn)
+{
+	int c, sign, mark;
+	float fract;
+
+	while (isspace(c = getch())) //skip white space
+		;
+	if (!isdigit(c) && c != EOF && c != '+' && c != '-' && c != '.')
+	{
+		ungetch(c); //it's not a number
+		return 0;
+	}
+	sign = (c == '-') ? -1 : 1;
+	if (c == '+' || c == '-')
+	{
+		mark = c; //remember a sign
+		c = getch();
+		if (!isdigit(c))
+		{
+			if (c != EOF)
+				ungetch(c); //not a number - back on the input
+			ungetch(mark); //back sign of the symvol on the input
+			return 0;
+		}
+	}
+	for (*pn = 0.0; isdigit(c); c = getch())
+		*pn = 10.0 * *pn + (c - '0'); // collect integer part
+	if (c == '.')
+		c = getch();
+	for (fract = 1.0; isdigit(c); c = getch())
+	{
+		*pn = 10.0 * *pn + (c - '0');
+		fract *= 10.0;
+	}
+
+	*pn *= sign / fract;
+	if (c != EOF)
+		ungetch(c);
+	return c;
+}
