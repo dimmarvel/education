01faacc451f769319a51bfe131360dcdfea5e1e5
Dmitry <dimmarvel@mail.ru>
2020-07-17 17:43:28 +0300

    insert sort traning

--- С++/Srot on C++/insert sort.cpp
+++ С++/Srot on C++/insert sort.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void insert_sort1()
+{
 	int n;
 	cin >> n;
 	int*arr = new int[n];
@@ -23,8 +24,43 @@ int main() {
 	}
 	cout << endl;
 	for (int i = 0; i < n; i++)
-	{ 
+	{
 		cout << i << ") " << arr[i] << endl;
 	}
+}
+void insert_sort2()
+{
+	int size;
+	cin >> size;
+	int* arr = new int[n];
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 150 + 100;
+		cout << "#" << i << " - " << arr[i] << endl;
+	}
+
+	for (int top = 1; top < size; top++) {
+		int k = top;
+		
+		while (k > 0 && arr[k - 1] > arr[k]) {
+			int temp;
+
+			temp = arr[k];
+			arr[k] = arr[k - 1];
+			arr[k - 1] = temp;
+			k -= 1;
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < size; i++)
+	{
+		cout << "#" << i << " - " << arr[i] << endl;
+	}
+}
+
+
+int main() {
+	insert_sort2();
 	system("pause");
 }
