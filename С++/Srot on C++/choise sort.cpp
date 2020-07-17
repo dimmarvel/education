7c0fe9012314687046977516542e0b2941421495
Dmitry <dimmarvel@mail.ru>
2020-07-17 17:45:56 +0300

    choise sort traning

--- С++/Srot on C++/choise sort.cpp
+++ С++/Srot on C++/choise sort.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+void choise_sort1()
+{
 	int n;
 	cin >> n;
 	int*arr = new int[n];
@@ -11,18 +12,53 @@ int main() {
 		cout << i << ") " << arr[i] << endl;
 	}
 
-	for (int i = 0; i < n-1; i++) {
-		for (int j = i+1; j < n ;j++)
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++)
 		{
 			if (arr[j] < arr[i]) {
 				int temp = arr[j];
 				arr[j] = arr[i];
 				arr[i] = temp;
-	}	}	}
+			}
+		}
+	}
 	cout << endl;
 	for (int i = 0; i < n; i++)
 	{
 		cout << i << ") " << arr[i] << endl;
 	}
+}
+
+void choise_sort2()
+{
+	int n;
+	cin >> n;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 50 + 1;
+		cout << i << ") " << arr[i] << endl;
+	}
+
+	for (int i = 0; i < n - 1; i++) {
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i]) {
+				int temp = arr[j];
+
+				arr[j] = arr[i];
+				arr[i] = temp;
+			}
+		}
+	}
+	cout << endl;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "#" << i << "-> " << arr[i] << endl;
+	}
+}
+int main() {
+	choise_sort2();
 	system("pause");
 }
