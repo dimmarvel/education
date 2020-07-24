6fdf021f9c665153ffb3617386fd589337ec6f00
Dmitry <dimmarvel@mail.ru>
2020-07-24 17:53:04 +0300

    bubble sort

--- С++/Srot on C++/bubble sort.cpp
+++ С++/Srot on C++/bubble sort.cpp
@@ -1,28 +1,6 @@
 #include <iostream>
 using namespace std;
-void swap(int*, int*);
-void buble_sort(int*, int);
-void coutArr(int* arr, int size);
-int main()
-{
-	setlocale(LC_ALL, "Rus");
-	int siz;
-	cin >> siz;
-	int*arr = new int[siz];
-
-	for (int i = 0; i < siz; i++)
-	{
-		arr[i] = rand() % 100 + 1;
-		cout << "n -" << i << "- = " << arr[i] << endl;
-	}
-	buble_sort(arr, siz);
-	cout << endl;
-	coutArr(arr, siz);
-	cout << endl;
-	system("pause");
-	return 0;
-}
-void buble_sort(int* arr, int size)
+void bubble_sort1(int* arr, int size)
 {
 	for (int i = 1; i < size; i++)
 	{
@@ -34,6 +12,20 @@ void buble_sort(int* arr, int size)
 		}
 	}
 }
+
+void buble_sort2(int* arr, int size)
+{
+	for (int i = 1; i < size; i++)
+	{
+		for (int j = 0; j < size - i; j++)
+		{
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+}
+
 void coutArr(int* arr, int size)
 {
 	for (int i = 0; i < size; i++)
@@ -41,9 +33,30 @@ void coutArr(int* arr, int size)
 		cout << "n -" << i << "- = " << arr[i] << endl;
 	}
 }
+
 void swap(int* srt, int* srt2)
 {
 	int temp = *srt;
 	*srt = *srt2;
 	*srt2 = temp;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int siz;
+	cin >> siz;
+	int* arr = new int[siz];
+
+	for (int i = 0; i < siz; i++)
+	{
+		arr[i] = rand() % 100 + 1;
+		cout << "n -" << i << "- = " << arr[i] << endl;
+	}
+	buble_sort2(arr, siz);
+	cout << endl;
+	coutArr(arr, siz);
+	cout << endl;
+	system("pause");
+	return 0;
 }
