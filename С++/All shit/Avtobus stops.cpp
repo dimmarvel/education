2ad1c79dcbdf31395f0d2fdb9044a2e2911759da
Dmitry <dimmarvel@mail.ru>
2020-07-25 23:12:56 +0300

    Avtobus stops

---
+++ С++/All shit/Avtobus stops.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+
+using namespace std;
+
+int main()
+{
+	map<int,vector<string>> bus;
+	int size;
+	int counter = 0;
+	cin >> size;
+
+	for (int i = 0; i < size; i++)
+	{
+		int how;
+		vector<string> stops;
+		string stop;
+
+		cin >> how;
+
+		for (int i = 0; i < how; i++)
+		{
+			cin >> stop;
+			stops.push_back(stop);
+		}
+		
+		bool is_here = false;
+		for (const auto& it : bus)
+		{
+			if (it.second == stops)
+			{
+				is_here = true;
+				cout << "Already exists for " << it.first << endl;
+				break;
+			}
+		}
+
+		if (!is_here)
+		{
+			bus[++counter] = stops;
+			cout << "New bus " << counter << endl;
+		}
+	}
+}
