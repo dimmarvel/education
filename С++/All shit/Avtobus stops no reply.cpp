89fc160496ff8bc3c5c6fb1b8d4a3bd7b98fc9fe
Dmitry <dimmarvel@mail.ru>
2020-07-27 23:55:58 +0300

    Avtobus stops no reply

---
+++ С++/All shit/Avtobus stops no reply.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<set>
+
+using namespace std;
+
+// справочник стран\столиц
+
+int main()
+{
+	map<int,set<string>> bus;
+	int size;
+	int counter = 0;
+	cin >> size;
+
+	for (int i = 0; i < size; i++)
+	{
+		int how;
+		set<string> stops;
+		string stop;
+
+		cin >> how;
+
+		for (int i = 0; i < how; i++)
+		{
+			cin >> stop;
+			stops.insert(stop);
+		}
+		
+		bool is_here = false;
+		for (const auto& it : bus)
+		{
+
+			if (it.second == stops)
+			{
+				is_here = true;
+				cout << "Already exists for " << it.first << endl;
+				break;
+			}
+		}
+
+		if (!is_here)
+		{
+			bus[++counter] = stops;
+			cout << "New bus " << counter << endl;
+		}
+	}
+}
