96c855fd65f9f6dbaf020eeaeaf74d006b06d22e
Dmitry <dimmarvel@mail.ru>
2020-07-28 22:37:21 +0300

    Class sort strings

---
+++ С++/All shit/Class sort strings.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class SortedStrings {
+public:
+	void AddString(const string& s) {
+		str.push_back(s);
+	}
+	vector<string> GetSortedStrings() {
+		sort(begin(str), end(str));
+		return str;
+	}
+private:
+	vector<string> str;
+};
+
+void PrintSortedStrings(SortedStrings& strings) {
+	for (const string& s : strings.GetSortedStrings()) {
+		cout << s << " ";
+	}
+	cout << endl;
+}
+
+int main() {
+	SortedStrings strings;
+
+	strings.AddString("first");
+	strings.AddString("third");
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+
+	strings.AddString("second");
+	PrintSortedStrings(strings);
+
+	return 0;
+}
