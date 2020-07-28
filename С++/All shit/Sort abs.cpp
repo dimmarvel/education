e260b7f0e1f3e0bb045184dfca156d1c82bd2c5d
Dmitry <dimmarvel@mail.ru>
2020-07-28 22:34:02 +0300

    Sort abs

---
+++ С++/All shit/Sort abs.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+#include<map>
+#include<vector>
+#include<algorithm>
+#include<set>
+
+using namespace std;
+
+// справочник стран\столиц
+
+int main()
+{
+	vector<int> v;
+	int size = 0;
+	cin >> size;
+	v.reserve(size + 1);
+	
+	for (int i = 0; i < size; i++)
+	{
+		int num;
+		cin >> num;
+		v.push_back(num);
+	}
+	sort(begin(v), end(v), [](int a, int b) { return abs(a) < abs(b); });
+	for (const auto& it : v)
+	{
+		cout << it << " ";
+	}
+}
