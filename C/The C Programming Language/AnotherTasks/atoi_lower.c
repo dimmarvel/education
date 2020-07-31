8902de40fd597169f55dbab46968fd374c6471a9
Dima Matsiukhov <dimmarvel@mail.ru>
2020-07-31 15:02:06 +0300

    atoi_lower

---
+++ C/The C Programming Language/AnotherTasks/atoi_lower.c
@@ -0,0 +1,21 @@
+int atoi(char s[])
+{
+	int i, n;
+	n = 0;
+
+	for (i = 0; s[i] >= '0' && s[i] <= '9'; i++)
+		n = 10 * n + (s[i] - '0');
+	return n;
+}
+
+int lower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return c + 'a' - 'A';
+	}
+	else
+	{
+		return c;
+	}
+}
