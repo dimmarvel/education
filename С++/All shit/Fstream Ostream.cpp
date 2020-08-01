faa71dbf9fa7188c0ccccfa3ecb18c4ebff26eb2
Dmitry <dimmarvel@mail.ru>
2020-08-01 13:21:51 +0300

    Fstream Ostream

---
+++ С++/All shit/Fstream Ostream.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
+
+void printFile(string path)
+{
+	ifstream file(path);
+	string line;
+	if (file)
+	{
+		while (getline(file, line))
+		{
+			cout << line << endl;
+		}
+	}
+}
+void printInFile(string opath,string ipath)
+{
+	ifstream ifile(ipath);
+	ofstream ofile(opath);
+	string line;
+	if (ifile)
+	{
+		while (getline(ifile, line))
+		{
+			ofile << line << endl;
+		}
+	}
+}
+
+int main()
+{
+	string path = "input.txt";
+	string pathout = "output.txt";
+	printFile(path);
+	printInFile(pathout, path);
+	return 0;
+}
