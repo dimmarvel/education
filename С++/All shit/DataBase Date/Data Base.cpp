2178e51044499237257c53933a5877050ba0e68c
Dima Matsiukhov <dimmarvel@mail.ru>
2020-08-17 18:05:24 +0300

    Data Base(fix bug and parsing rewrote)

--- С++/All shit/DataBase Date/Data Base.cpp
+++ С++/All shit/DataBase Date/Data Base.cpp
@@ -3,7 +3,7 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <vector>
+#include <set>
 #include <map>
 #include <iomanip>
 
@@ -24,9 +24,16 @@ public:
 	int GetMonth() const;
 	int GetDay() const;
 	void Input(istream& ifstr);
-	void Out() const;
+	friend ostream& operator<<(ostream& ostr,const Date& date);
 };
 
+ostream& operator<<(ostream& ostr, const Date& date)
+{
+	return ostr << setfill('0')
+		<< setw(4) << date.GetYear() << "-"
+		<< setw(2) << date.GetMonth() << "-"
+		<< setw(2) << date.GetDay();
+}
 int ConvertDateToDays(const Date& date)
 {
 	return date.GetDay() + date.GetMonth() * 31 + date.GetYear() * 365;
@@ -34,33 +41,29 @@ int ConvertDateToDays(const Date& date)
 
 void Date::Input(istream& ifstr)
 {
-	if (ifstr)
-	{
-		ifstr >> year;
-		ifstr.ignore(1);
+	string str_date;
+	char def1, def2;
 
-		ifstr >> month;
+	ifstr >> str_date;
+	stringstream temp(str_date);
+	temp.clear();
+	temp >> year >> def1 >> month >> def2 >> day;
+
+	if (temp && temp.eof() && def1 == '-' && def2 == '-')
+	{
 		if (month < 1 || month > 12)
 		{
 			throw runtime_error("Month value is invalid: " + to_string(month));
 		}
-
-		ifstr.ignore(1);
-		ifstr >> day;
 		if (day < 1 || day > 31)
 		{
 			throw runtime_error("Day value is invalid: " + to_string(day));
 		}
-
 	}
-}
-
-void Date::Out() const
-{
-	cout << setfill('0')
-		<< setw(4) << year << "-" 
-		<< setw(2) << month << "-" 
-		<< setw(2) << day;
+	else
+	{
+		throw runtime_error("Wrong date format: " + str_date);
+	}
 }
 
 int Date::GetDay() const
@@ -95,7 +98,7 @@ bool operator==(const Date& lhs, const Date& rhs)
 }
 
 class Database {
-	map<Date, vector<string>> data;
+	map<Date, set<string>> data;
 public:
 	void AddEvent(const Date& date, const string& event);
 	bool DeleteEvent(const Date& date, const string& event);
@@ -104,32 +107,15 @@ public:
 	void Find(const Date& date);
 
 	void Print() const;
-	friend istream& operator>>(istream& stream, Database& db);
 };
 
-istream& operator>>(istream& stream, Database& db)
-{
-	Date date;
-	date.Input(stream);
-	if (stream)
-	{
-		string ev;
-		stream >> ev;
-		db.data[date].push_back(ev);
-	}
-	else
-	{
-		db.data[date];
-	}
-	return stream;
-}
 
 void Database::AddEvent(const Date& date, const string& event)
 {
 	if (event != "")
 	{
-		data[date].push_back(event);
-		sort(data[date].begin(), data[date].end());
+		data[date].insert(event);
+		//sort(data[date].begin(), data[date].end());
 	}
 	else // if event not added
 	{
@@ -145,7 +131,7 @@ bool Database::DeleteEvent(const Date& date, const string& event)
 		data[date].erase(it);
 		return true;
 	}
-	else 
+	else
 	{
 		return false;
 	}
@@ -154,13 +140,13 @@ bool Database::DeleteEvent(const Date& date, const string& event)
 int Database::DeleteDate(const Date& date)
 {
 	int size = data[date].size();
-	data[date].clear();
+	data.erase(date);
 	return size;
 }
 
 void Database::Find(const Date& date)
 {
-	vector<string> events = data[date];
+	set<string> events = data[date];
 	for (auto& it : events)
 	{
 		cout << it << endl;
@@ -171,12 +157,10 @@ void Database::Print() const
 {
 	for (const auto& it : data)
 	{
-		it.first.Out();
 		for (const auto& vec : it.second)
 		{
-			cout << " " << vec;
+			cout << it.first << " " << vec << endl;;
 		}
-		cout << endl;
 	}
 }
 
@@ -188,63 +172,63 @@ int main() {
 	try {
 		while (getline(cin, line))
 		{
-				string command;
-				stringstream ss_line(line);
-				ss_line >> command;
+			string command;
+			stringstream ss_line(line);
+			ss_line >> command;
 
-				if (command == "Add")
+			if (command == "Add")
+			{
+				Date date;
+				string ev;
+				date.Input(ss_line);
+				ss_line >> ev;
+				db.AddEvent(date, ev);
+			}
+			else if (command == "Del")
+			{
+				string ev, date_str;
+				Date date;
+				date.Input(ss_line);
+				ss_line >> ev;
+				if (ev != "")
 				{
-					Date date;
-					string ev;
-					date.Input(ss_line);
-					ss_line >> ev;
-					db.AddEvent(date, ev);
-				}
-				else if (command == "Del")
-				{
-					string ev, date_str;
-					Date date;
-					date.Input(ss_line);
-					ss_line >> ev;
-					if (ev != "")
+					/*Команда должна удалить добавленное ранее событие с указанным именем в указанную дату,
+					если оно существует.Если событие найдено и удалено,
+					программа должна вывести строку «Deleted successfully»(без кавычек).
+					Если событие в указанную дату не найдено,
+					программа должна вывести строку «Event not found»(без кавычек).*/
+					if (db.DeleteEvent(date, ev))
 					{
-						/*Команда должна удалить добавленное ранее событие с указанным именем в указанную дату,
-						если оно существует.Если событие найдено и удалено,
-						программа должна вывести строку «Deleted successfully»(без кавычек).
-						Если событие в указанную дату не найдено,
-						программа должна вывести строку «Event not found»(без кавычек).*/
-						if (db.DeleteEvent(date, ev))
-						{
-							cout << "Deleted successfully" << endl;
-						}
-						else
-						{
-							cout << "Event not found" << endl;
-						}
+						cout << "Deleted successfully" << endl;
 					}
 					else
 					{
-						/*Команда удаляет все ранее добавленные события за указанную дату.
-						Программа всегда должна выводить строку вида «Deleted N events»,
-						где N — это количество всех найденных и удалённых событий.N может быть равно нулю,
-						если в указанную дату не было ни одного события.*/
-						cout << "Deleted " << db.DeleteDate(date) << " events" << endl;
+						cout << "Event not found" << endl;
 					}
-				}
-				else if (command == "Find")
-				{
-					Date date;
-					date.Input(ss_line);
-					db.Find(date);
-				}
-				else if (command == "Print")
-				{
-					db.Print();
 				}
 				else
 				{
-					cout << "Unknown command: " << command << endl;
+					/*Команда удаляет все ранее добавленные события за указанную дату.
+					Программа всегда должна выводить строку вида «Deleted N events»,
+					где N — это количество всех найденных и удалённых событий.N может быть равно нулю,
+					если в указанную дату не было ни одного события.*/
+					cout << "Deleted " << db.DeleteDate(date) << " events" << endl;
 				}
+			}
+			else if (command == "Find")
+			{
+				Date date;
+				date.Input(ss_line);
+				db.Find(date);
+			}
+			else if (command == "Print")
+			{
+				db.Print();
+			}
+			else if (command != "")
+			{
+				throw runtime_error("Unknown command: " + command);
+			}
 		}
 	}
 	catch (runtime_error& ex)
