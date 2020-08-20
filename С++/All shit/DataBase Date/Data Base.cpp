7b5f1f51a6a3956efd3173c56596c07c40390eaf
Dima Matsiukhov <dimmarvel@mail.ru>
2020-08-20 17:39:11 +0300

    Revert "Revert "Revert "Data Base(Add func -> add, del data/del data event, operator > < ==)"""
    This reverts commit 4bb9feab225a38dc05c308d431f393c802aaee03.

--- С++/All shit/DataBase Date/Data Base.cpp
+++ С++/All shit/DataBase Date/Data Base.cpp
@@ -1,8 +1,6 @@
-#include <string>
 #include <iostream>
-#include <algorithm>
 #include <fstream>
-#include <sstream>
+#include <string>
 #include <vector>
 #include <map>
 using namespace std;
@@ -13,6 +11,7 @@ using namespace std;
 //- поиск событий за конкретную дату : Find Дата
 //- печать всех событий за все даты : Print
 
+// Реализуйте функции и методы классов и при необходимости добавьте свои
 
 class Date {
 private:
@@ -23,19 +22,17 @@ public:
 	int GetYear() const;
 	int GetMonth() const;
 	int GetDay() const;
-	void Input(istream& ifstr);
+	void Input(ifstream& ifstr);
 	void Out() const;
 };
 
-void Date::Input(istream& ifstr)
+void Date::Input(ifstream& ifstr)
 {
 	if (ifstr)
 	{
-		ifstr >> year;
-		ifstr.ignore(1);
-		ifstr >> month;
-		ifstr.ignore(1);
-		ifstr >> day;
+		getline(ifstr, year, '-');
+		getline(ifstr, month, '-');
+		getline(ifstr, day, '-');
 	}
 }
 
@@ -62,26 +59,18 @@ int Date::GetYear() const
 
 bool operator<(const Date& lhs, const Date& rhs)
 {
-	int d1 = lhs.GetDay() + lhs.GetMonth() * 31 + lhs.GetYear() * 365;
-	int d2 = rhs.GetDay() + rhs.GetMonth() * 31 + rhs.GetYear() * 365;
-	
-	return d1 < d2;
-}
-
-bool operator>(const Date& lhs, const Date& rhs)
-{
-	int d1 = lhs.GetDay() + lhs.GetMonth() * 31 + lhs.GetYear() * 365;
-	int d2 = rhs.GetDay() + rhs.GetMonth() * 31 + rhs.GetYear() * 365;
-	
-	return d1 > d2;
-}
-
-bool operator==(const Date& lhs, const Date& rhs)
-{
-	int d1 = lhs.GetDay() + lhs.GetMonth() * 31 + lhs.GetYear() * 365;
-	int d2 = rhs.GetDay() + rhs.GetMonth() * 31 + rhs.GetYear() * 365;
-	
-	return d1 == d2;
+	if (lhs.GetYear() != rhs.GetYear())
+	{
+		return lhs.GetYear() < rhs.GetYear();
+	}
+	else if (lhs.GetMonth() != rhs.GetMonth())
+	{
+		return lhs.GetMonth() < rhs.GetMonth();
+	}
+	else if (lhs.GetDay() != rhs.GetDay())
+	{
+		return lhs.GetDay() < rhs.GetDay();
+	}
 }
 
 class Database {
@@ -94,63 +83,26 @@ public:
 	void Find(const Date& date) const;
 
 	void Print() const;
-	friend istream& operator>>(istream& stream, Database& db);
+	
 };
-
-istream& operator>>(istream& stream, Database& db)
-{
-	Date date;
-	date.Input(stream);
-	if (stream)
-	{
-		string ev;
-		stream >> ev;
-		db.data[date].push_back(ev);
-	}
-	else
-	{
-		db.data[date];
-	}
-	return stream;
-}
 
 void Database::AddEvent(const Date& date, const string& event)
 {
-	if (event != "")
-	{
-		data[date].push_back(event);
-	}
-	else
-	{
-		data[date];
-	}
+	data[date].push_back(event);
 }
 
 bool Database::DeleteEvent(const Date& date, const string& event)
 {
-	auto it = find(data[date].begin(), data[date].end(), event);
-	if (it != data[date].end())
-	{
-		data[date].erase(it);
-		return true;
-	}
-	else 
-	{
-		return false;
-	}
-
 
 }
 
-int Database::DeleteDate(const Date& date)
-{
-	int size = data[date].size();
-	data[date].clear();
-	return size;
-}
-void Database::Find(const Date& date) const
+int  Database::DeleteDate(const Date& date)
 {
 
+}
+void Database::Find(const Date& date) const 
+{
+	
 }
 void Database::Print() const
 {
@@ -161,60 +113,30 @@ void Database::Print() const
 		{
 			cout << " " << vec;
 		}
-		cout << endl;
 	}
 }
 
 int main() {
 	Database db;
 
-	string line;
+	string command;
 	try {
-		while (getline(cin, line))
-		{
-			string command;
-			stringstream ss_line(line);
-			ss_line >> command;
-
+		while (getline(cin, command)) {
 			if (command == "Add")
-			{
-				Date date;
-				string ev;
-				date.Input(ss_line);
-				ss_line >> ev;
-				db.AddEvent(date, ev);
-			}
-			
-			else if (command == "Del")
 			{
 				string ev, date_str;
 				Date date;
-				date.Input(ss_line);
-				ss_line >> ev;
-				if (ev != "")
-				{
-					/*Команда должна удалить добавленное ранее событие с указанным именем в указанную дату,
-					если оно существует.Если событие найдено и удалено,
-					программа должна вывести строку «Deleted successfully»(без кавычек).
-					Если событие в указанную дату не найдено,
-					программа должна вывести строку «Event not found»(без кавычек).*/
-					if (db.DeleteEvent(date, ev))
-					{
-						cout << "Deleted successfully" << endl;
-					}
-					else
-					{
-						cout << "Event not found" << endl;
-					}
-				}
-				else
-				{
-					/*Команда удаляет все ранее добавленные события за указанную дату.
-					Программа всегда должна выводить строку вида «Deleted N events», 
-					где N — это количество всех найденных и удалённых событий.N может быть равно нулю, 
-					если в указанную дату не было ни одного события.*/
-					cout << "Deleted "<< db.DeleteDate(date) << " events" << endl;
-				}
+				cin >> date_str;
+				ifstream ifstr(date_str);
+
+				date.Input(ifstr);
+				cin >> ev;
+
+				db.AddEvent(date, ev);
+			}
+			else if (command == "Del")
+			{
+
 			}
 			else if (command == "Find")
 			{
@@ -222,7 +144,7 @@ int main() {
 			}
 			else if (command == "Print")
 			{
-				db.Print();
+
 			}
 		}
 	}
