0cee4bb0b8f75d9116385328bc5fdb9d36ca6517
Dima Matsiukhov <dimmarvel@mail.ru>
2020-09-08 14:57:39 +0300

    Ex 6 5

---
+++ C/The C Programming Language/Ex 6 5.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+struct nlist{
+	struct nlist * next;
+	char * name;
+	char * defn;
+
+};
+
+#define HASHSIZE 101
+
+static struct nlist *hashtab[HASHSIZE];
+
+unsigned hash(char * s)
+{
+	unsigned hashval;
+
+	for (hashval = 0; *s != '\0'; s++)
+		hashval = *s + 31 * hashval;
+
+	return hashval % HASHSIZE;
+}
+
+struct nlist * lookup(char* s)
+{
+	struct nlist *np;
+	
+	for (np = hashtab[hash(s)]; np != NULL; np = np->next)
+		if (strcmp(s, np->name))
+			return np;
+
+	return NULL;
+}
+
+struct nlist* install(char* name, char * defn)
+{
+	struct nlist * np;
+	unsigned hashval;
+
+	if ((np = lookup(name)) == NULL)
+	{
+		np = (struct nlist*)malloc(sizeof(*np));
+		if (np == NULL || (np->name = strdup(name)) == NULL)
+			return NULL;
+
+		hashval = hash(name);
+		np->next = hashtab[hashval];
+		hashtab[hashval] = np;
+	}
+	else
+	{
+		free((void*)np->defn);
+	}
+	
+	if ((np->defn = strdun(defn)) == NULL)
+	{
+		return NULL;
+	}
+	return np;
+}
+
+main()
+{
+
+	return 0;
+
+}
