fad0da13b537128b54ac5cb9d916a125348f6bfd
Dima Matsiukhov <dimmarvel@mail.ru>
2020-09-16 11:24:37 +0300

    Scanf(date input)

---
+++ C/The C Programming Language/AnotherTasks/Scanf(date input).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+main()
+{
+	int day, year;
+	char monthname[20];
+	char line[100];
+	while (getline(line, sizeof(line)) > 0)
+	{
+		if (sscanf(line, "%d %s %d", &day, monthname, &year) == 3)
+		{
+			printf("valid: %s", line);
+		}
+		else if (sscanf(line, "%d/%s/%d", &day, monthname, &year) == 3)
+		{
+			printf("valid: %s", line);
+		}
+		else
+		{
+			printf("error: %s", line);
+		}
+	}
+}
