b7ba9ba02f1778a7109a08d96eaf704f95160250
Dima Matsiukhov <dimmarvel@mail.ru>
2020-09-22 15:02:18 +0300

    Ex 8 1

---
+++ C/The C Programming Language/Ex 8 1.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include "syscalls.h"
+
+int main(int argc, char *argv[])
+{
+    int fp;
+    void filecopy(int ifp, int ofp);
+   
+    if(argc == 1) //no arguments, default stdin
+        filecopy(0, 1);
+    else
+        while(--argc > 0)
+            if((fp = open(*++argv, 0_RDONLY)) == -1)
+                error("cat: can`t open the file %s\n", *argv);
+            else
+            {
+                filecopy(fp, 1);
+                close(fp);
+            }
+    return 0;
+}
+
+void filecopy(int ifp, int ofp)
+{
+    int n;
+    char buf[BUFSIZ];
+   
+    while((n = read(ifp, buf, BUFSIZ)) > 0)
+        if(write(ofp, buf, n) != n)
+            error("cat:write error");
+}
