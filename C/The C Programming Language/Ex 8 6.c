5bb992cd9ccb1fa917cda5cc50153bf465f6d187
Dima Matsiukhov <dimmarvel@mail.ru>
2020-09-27 14:47:53 +0300

    Ex 8 6

---
+++ C/The C Programming Language/Ex 8 6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void _calloc(unsigned n, unsigned size)
+{
+	unsigned i, nsize;
+	char *p, *q;
+
+	nsize = n * size;
+	if ((p = q = malloc(nsize)) != NULL)
+		for (i = 0; i < nsize; i++)
+			*q++ = 0;
+	return p;
+}
