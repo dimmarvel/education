872dedf2e8da51252c6f06211914ab0e749cd62e
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-04 10:37:25 +0300

    Ex 8 8

---
+++ C/The C Programming Language/Ex 8 8.c
@@ -0,0 +1,13 @@
+unsigned bfree(char *p, unsigned n)
+{
+	Header *hp;
+
+	if (n < sizeof(Header))
+		return 0;
+
+	hp = (Header *)p;
+	hp->s.size = n / sizeof(Header);
+	free((void *)(hp + 1));
+	
+	return hp->s.size;
+}
