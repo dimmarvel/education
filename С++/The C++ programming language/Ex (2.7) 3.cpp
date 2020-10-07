215d7da1f68c0d5c2d55ae00f3009cbe5a1a9048
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-07 12:44:43 +0300

    Ex(2.7) 3

---
+++ С++/The C++ programming language/Ex (2.7) 3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+char some;
+char temp[1][2];
+int cs;
+
+char* qw = &some;
+int arr[3] = {1,2,3};
+int& q = arr[0];
+char* arr[2];
+char** s = &qw;
+const int a = 1;
+const int*  b = &a;
+int *const w = &cs;
