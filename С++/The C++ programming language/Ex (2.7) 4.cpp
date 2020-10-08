1d2411e9baa608bcb3ece98308d15cf56c923158
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-08 12:53:05 +0300

    Ex(2.7) 4

---
+++ С++/The C++ programming language/Ex (2.7) 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "double - " << sizeof(double) << endl;
+	cout << "int - " << sizeof(int) << endl;
+	cout << "char - " << sizeof(char) << endl;
+	cout << "float - " << sizeof(float) << endl;
+	cout << "long - " << sizeof(long) << endl;
+	system("pause");
+}
