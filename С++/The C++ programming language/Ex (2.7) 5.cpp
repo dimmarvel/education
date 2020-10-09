b334638d555f57b213b68e6c564db0c5218276b0
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-09 17:41:45 +0300

    Ex(2.7) 5

---
+++ С++/The C++ programming language/Ex (2.7) 5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 'a'; i <= 'z'; i++)
+	{
+		cout << (char)i << " - " << i << " - 0x" << hex << i <<endl;
+	}
+	for (int i = '0'; i <= '9'; i++)
+	{
+		cout << (char)i << " - " << i << " - 0x" << hex <<  i << endl;
+	}
+	system("pause");
+}
