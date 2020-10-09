501c4dd03a2d37027dffa662f08fec6fcd0d0ebd
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-09 17:45:25 +0300

    Ex(2.7) 6

---
+++ С++/The C++ programming language/Ex (2.7) 6.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	struct fudge {
+		union {
+			int i;
+			int* p;
+		};
+	};
+	fudge a;
+	a.i = 4095;
+	int* p = a.p;
+
+	a.p = 0;
+	cout << a.p << endl;
+	system("pause");
+}
