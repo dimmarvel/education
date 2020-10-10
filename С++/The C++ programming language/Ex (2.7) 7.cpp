c6b1703516ba585268f25e2a25f37648357b1d35
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-10 09:31:17 +0300

    Ex(2.7) 7

---
+++ С++/The C++ programming language/Ex (2.7) 7.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+typedef union {
+	double f;
+	struct {
+		unsigned int mantisa : 23;
+		unsigned int exponent : 8;
+		unsigned int sign : 1;
+	} parts;
+} double_cast;
+void manticce(double m)
+{
+	double_cast dc = {m};
+	printf("%x\n", dc.parts.mantisa);
+}
+int main()
+{
+	manticce(1.21411577);
+	system("pause");
+}
