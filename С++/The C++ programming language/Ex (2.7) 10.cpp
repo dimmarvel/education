fa7f3f95c1229d2d38a6d7aa2a736a0de283f5ee
Dima Matsiukhov <dimmarvel@mail.ru>
2020-10-11 08:55:20 +0300

    Ex(2.7) 10

---
+++ С++/The C++ programming language/Ex (2.7) 10.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+void pswap(int* first, int* sec)
+{
+	int temp = *first;
+	*first = *sec;
+	*sec = temp;
+}
+void lswap(int& first, int& sec)
+{
+	int temp = first;
+	first = sec;
+	sec = temp;
+}
+
+int main()
+{
+	int f = 10, s = 20;
+	cout << "f = " << f << ", " << "s = " << s << endl;
+	pswap(&f, &s);
+	cout << "pswap f = " << f << ", " << "s = " << s << endl;
+	lswap(f, s);
+	cout << "lswap f = " << f << ", " << "s = " << s << endl;
+	system("pause");
+}
