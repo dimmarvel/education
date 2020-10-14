cab46cec7f74dd6fe9208b038a843c31236fc3e8
dimmarvel <dimmarvel@mail.ru>
2020-10-14 18:22:58 +0300

    Ex(2.7) 13

---
+++ С++/The C++ programming language/Ex(2.7) 13.cpp
@@ -0,0 +1,9 @@
+typedef unsigned char uchar;
+typedef const unsigned char cuchar;
+
+typedef int* pint;
+typedef char** ppchar;
+typedef char* pachar[];
+typedef int* paint[7];
+typedef int** ppaint[7];
+typedef int* arr[8][7];
