df1bd53d5a8eaf4e3c68ddd1425f03f65582c71b
dimmarvel <dimmarvel@mail.ru>
2020-10-22 18:14:56 +0300

    Ex(3.5) 1(add pointer)

--- С++/The C++ programming language/Ex (3.5) 1.cpp
+++ С++/The C++ programming language/Ex (3.5) 1.cpp
@@ -2,13 +2,21 @@
 
 int main()
 {
-	int max_length = 100;
+	int max_length = 10;
 	int i = 0;
 	int quest_count = 0;
-	char input_line[max_length] = "hey? how are you?";
+	char input_line[max_length] = "hey?im?qw";
+
 	while(i < max_length)
-	{
 		if(input_line[i++] == '?') quest_count++;
+	std::cout << "quest_cout(while): " << quest_count << std::endl;
+
+	quest_count = 0;
+	for(char* p = input_line; p < &input_line[max_length]; p++)
+	{
+		if(*p == '?') quest_count++;
+		std::cout << *p << std::endl;
 	}
-	std::cout << "quest_cout: " << quest_count << std::endl;
+	std::cout << "quest_cout(pointer): " << quest_count << std::endl;
 }
+
