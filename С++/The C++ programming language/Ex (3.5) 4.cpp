04dd20b0e6f99b6382822561805cac3fec82e7b0
dimmarvel <dimmarvel@mail.ru>
2020-10-23 07:56:41 +0300

    Ex (3.5) 4

---
+++ С++/The C++ programming language/Ex (3.5) 4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+	f(v[i], i++); //v[i] OR i++ was called first
+	int some = 102491294; // 8/16/32 cant take this numb in int
+	std::system("pause"); // not find on linux
+	char c = 255;
+	if(c == -1) //sygnal machine error
+		
+	
+}
+
