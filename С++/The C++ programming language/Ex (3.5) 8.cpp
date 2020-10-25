2a31b1204f3488df9b4ba14c36192f5d05bb3766
dimmarvel <dimmarvel@mail.ru>
2020-10-25 16:21:47 +0300

    Ex (3.5) 8

---
+++ С++/The C++ programming language/Ex (3.5) 8.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+void f(int a, int b)
+{
+	if(a = 3){} //warning: suggest parentheses around assignment
+				//used as truth value [-Wparentheses]
+	if(a&007==0){} 	//warning: suggest parentheses around
+					//comparison in operand of ‘&’ [-Wparentheses]
+	a := b+1; //error: expected primary-expression before ‘=’ token
+
+}
+
