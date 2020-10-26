c48d35b4b3c932a81f450065ed1125ffad4735a9
dimmarvel <dimmarvel@mail.ru>
2020-10-26 17:06:44 +0300

    Ex (3.5) 10

---
+++ С++/The C++ programming language/Ex (3.5) 11.cpp
@@ -0,0 +1,30 @@
+#include<cstdio>
+#include<string>
+#include<algorithm>
+
+using namespace std;
+
+int atoi (string s)
+{
+  int res = 0;
+  int n = s.length();
+  for (int i = 0; i < n; i++)
+  {
+    res *= 10;
+    res += s[i] - '0';
+  }
+  return res;
+}
+
+string itoa (int n)
+{
+  string res = "";
+  while (n > 0)
+  {
+    res += '0' + n % 10;
+    n /= 10;
+  }
+  reverse(res.begin(), res.end());
+  return res;
+}
+
