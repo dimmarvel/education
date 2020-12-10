aac123aa20948f23d7e95806ea67b4f821e9cbb6
dimmarvel <dimmarvel@mail.ru>
2020-12-10 11:42:56 +0300

    File linker

---
+++ С++/The C++ programming language/Examples/Link/file1.cpp
@@ -0,0 +1,7 @@
+int a = 10;
+const double b = 9; // important
+typedef int I;
+
+int f(){
+	return a;
+}
---
+++ С++/The C++ programming language/Examples/Link/file2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+typedef int I;
+
+extern int f();
+extern int a;
+extern const double b = 9;
+//Константа может получить внешнее связывание только с помощью явного описания:
+
+int main()
+{
+	std::cout << f() << std::endl;
+	std::cout << a << std::endl;
+	std::cout << b << std::endl;
+	return 0;
+}
