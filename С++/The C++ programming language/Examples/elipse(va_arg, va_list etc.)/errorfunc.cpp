43deb678d760ecc7861fbdac2531570266cc4a82
dimmarvel <dimmarvel@mail.ru>
2020-12-13 10:32:09 +0300

    elipse(va_arg, va_list, va_start etc.)

---
+++ С++/The C++ programming language/Examples/elipse(va_arg, va_list etc.)/errorfunc.cpp
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+#include <iostream>
+using namespace std;
+
+void error(int severity ...)
+{
+// severity степень тяжести ошибки, дальше список сток
+// завершающийся нулем
+	va_list ap;
+	va_start(ap, severity);
+// начало параметров
+	for (;;) {
+		char* p = va_arg(ap, char*);
+		if (p == 0) break;
+		cerr << p << ' ';
+	}
+	va_end(ap);
+// очистка параметров
+	cerr << '\n';
+	if (severity) exit(severity);
+}
