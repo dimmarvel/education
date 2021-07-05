bd68bea51719283bd86292f525cfcae3e4506a00
Dmitry <dimmarvel@mail.ru>
2021-07-05 22:58:00 +0300

    Clear repos

--- C++/All shit/Void pointer.cpp
+++ C++/All shit/Void pointer.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
 #include<iomanip>
+
 using namespace std;
 typedef bool(*howsort2)(int, int);
+
 bool bolshe(int a, int b) {
 	return a > b;
 }
+
 bool menshe(int a, int b) {
 	return a < b;
 }
+
 void sort(int *arr,int size, bool(*howsort)(int, int) = bolshe) {
 	for (int i = 0; i < size; i++)
 	{
@@ -18,6 +22,7 @@ void sort(int *arr,int size, bool(*howsort)(int, int) = bolshe) {
 		}
 	}
 }
+
 void sort2(int *arr,int size, howsort2 pointerOnFunc = bolshe) {
 	for (int i = 0; i < size; i++)
 	{
@@ -28,6 +33,7 @@ void sort2(int *arr,int size, howsort2 pointerOnFunc = bolshe) {
 		}
 	}
 }
+
 int main() {
 	int arr[8] = {1, 2, 5, 17, 8, 7, 20, 5};
 	for (int i = 0; i < 8; i++)
