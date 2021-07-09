454ba65bfd8726d7d61b1464cbc08e1c97f32f94
Dmitry <dimmarvel@mail.ru>
2021-07-09 22:27:45 +0300

    Fix

---
+++ C++/Algorithms/complexity of algorithms/task 3/resolve 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<int> vec = {1,2,3,2,1,2,4};
+    int ans = -1;
+    int input;
+
+    cin >> input;
+
+    for (int i = 0; i < vec.size(); ++i)
+        if(vec[i] == input)
+            ans = i;
+
+    cout << ans << endl;
+
+    return 0;
+}
