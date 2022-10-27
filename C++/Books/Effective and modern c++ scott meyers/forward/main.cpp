c3131851ba2d84182287182c0ad1a2a9380548e2
dimmarvel <dimmarvel@mail.ru>
2022-10-28 00:31:23 +0300

    [ADD] Forward example

---
+++ C++/Books/Effective and modern c++ scott meyers/forward/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+
+class Widget
+{
+public:
+    int a;
+    std::vector<int> b{10};
+};
+
+void process(const Widget& lvalArg) // lvalue
+{
+    std::cout << "lval" << std::endl;
+    return;
+}  
+
+void process(Widget&& rvalArg) // rvalue
+{
+    std::cout << "rval" << std::endl;
+    return;
+}       
+
+template<typename T>
+void logAndProcess(T&& param)
+{
+    process(std::forward<T>(param));
+    process(param);
+}
+
+int main()
+{
+    Widget w;
+    std::cout << "call logAndProcess with lval" << std::endl;
+    logAndProcess(w);
+    std::cout << "call logAndProcess with rval" << std::endl;
+    logAndProcess(std::move(w));
+
+    return 0;
+}
--- C++/Books/Effective and modern c++ scott meyers/move/main.cpp
+++ C++/Books/Effective and modern c++ scott meyers/move/main.cpp
@@ -1,13 +1,19 @@
 #include <type_traits>
 #include <string>
 #include <iostream>
-
+#include <vector>
 template <typename T>
 decltype(auto)
 sm_move(T&& param)
 {
     using ResultType = typename std::remove_reference<T>::type&&;
     return static_cast<ResultType>(param);
+}
+
+void f(std::vector<int>& v)
+{
+    std::vector<int> v2{1,2,3};
+    v = v2;
 }
 
 int main()
@@ -21,5 +27,19 @@ int main()
     std::cout << "after move:" << std::endl;
     std::cout << "s = " << s << std::endl;
     std::cout << "st = " << st << std::endl;
+
+    std::vector<int> v{1,2,3,4};
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " ";
+    }
+    
+    f(v);
+    std::cout << "after" << std::endl;
+    for (size_t i = 0; i < v.size(); i++)
+    {
+        std::cout << v[i] << " ";
+    }
+
     return 0;
 }
