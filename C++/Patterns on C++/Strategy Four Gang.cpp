58a17053d718806c117556843e867884013158c5
Dmitry Matsiukhov <dimmarvel@mail.ru>
2023-03-08 21:36:05 +0300

    [PATTERNS] Strategy with objects

--- C++/Patterns on C++/Strategy Four Gang.cpp
+++ C++/Patterns on C++/Strategy Four Gang.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+// FIRST IMPL -----------------------------------------------------------------
 class ZIP_compression
 {
 public:
@@ -23,6 +23,39 @@ public:
 private:
     strategy strat;
 };
+// FIRST IMPL -----------------------------------------------------------------
+
+// SECOND IMPL -----------------------------------------------------------------
+class compositor {
+public:
+    virtual int compose(int some_data) = 0;
+protected:
+    compositor() {}
+};
+
+class simple_compositor : public compositor
+{
+public:
+    simple_compositor() {}
+    virtual int compose(int some_data) { cout << "text_compositor" << endl; return some_data + 1; }
+};
+
+class text_compositor : public compositor
+{
+public:
+    text_compositor() {}
+    virtual int compose(int some_data) { cout << "text_compositor" << endl; return some_data + 2; }
+};
+
+class composition {
+public:
+    composition(compositor* cmp) : _compositor(cmp) {}
+    int repair() { return _compositor->compose(1); }
+
+private:
+    compositor* _compositor;
+};
+// SECOND IMPL -----------------------------------------------------------------
 
 int main()
 {
@@ -30,5 +63,9 @@ int main()
     context<ARJ_compression> cntx2;
     cntx1.operatiorn();
     cntx2.operatiorn();
+    composition* quick = new composition(new simple_compositor);
+    composition* slick = new composition(new text_compositor);
+    cout << "data " << quick->repair() << endl;
+    cout << "data " << slick->repair() << endl;
     return 0;
 }
