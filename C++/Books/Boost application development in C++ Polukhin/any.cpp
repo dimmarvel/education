9b452fde227dd70d94a54a9d1868d3f78c6433b1
Dmitry Matsiukhov <dimmarvel@mail.ru>
2023-05-14 22:52:48 +0300

    [BOOSTBOOK] add any exaample with variant

--- C++/Books/Boost application development in C++ Polukhin/any.cpp
+++ C++/Books/Boost application development in C++ Polukhin/any.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
+#include <boost/variant.hpp>
 #include <boost/any.hpp>
+#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <typeinfo>
+#include <string>
 
 typedef boost::any cell_t;
 typedef std::vector<cell_t> db_row_t;
+typedef boost::variant<int, float, std::string> v_cell_t;
+typedef std::vector<v_cell_t> v_db_row_t;
 
 db_row_t get_row(const char* query)
 {
@@ -13,6 +17,14 @@ db_row_t get_row(const char* query)
     row.push_back(10);
     row.push_back(10.1f);
     row.push_back(std::string("hello again"));
+    return row;
+}
+
+v_db_row_t v_get_row(const char* query) {
+    v_db_row_t row;
+    row.push_back(10);
+    row.push_back(10.1f);
+    row.push_back("hello again");
     return row;
 }
 
@@ -40,6 +52,25 @@ public:
     }
 };
 
+struct db_sum_visitor : public boost::static_visitor<double>
+{
+    double operator()(int value) const
+    {
+        return value;
+    }
+
+    double operator()(float value) const
+    {
+        return value;
+    }
+
+    double operator()(const std::string& value) const 
+    {
+        return 0.0;
+    }
+
+};
+
 void example1()
 {
     std::vector<boost::any> some_val;
@@ -58,7 +89,21 @@ void example2()
     db_row_t row = get_row("Query: Five me some row, please.");
     double res = 0.0;
     std::for_each(row.begin(), row.end(), db_sum(res));
-    std::cout << "Sum of arithmetic types in database row is: " << res << std::endl;
+    std::cout << "Sum of arithmetic any types in database row is: " << res << std::endl;
+    std::cout << "-------------------" << std::endl;
+}
+
+void example3()
+{
+    v_db_row_t row = v_get_row("Query: Five me some row, please.");
+    double res = 0.0;
+    
+    for (auto it = row.begin(), end = row.end(); it != end; ++it)
+    {
+        res += boost::apply_visitor(db_sum_visitor(), *it);
+    }
+
+    std::cout << "Sum of arithmetic variant types in database row is: " << res << std::endl;
     std::cout << "-------------------" << std::endl;
 }
 
@@ -66,5 +111,6 @@ int main(int argc, char* argv[])
 {
     example1();
     example2();
+    example3();
     return 0;
 }
