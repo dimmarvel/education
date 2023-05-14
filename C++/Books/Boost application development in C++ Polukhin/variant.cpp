9811ff927ed1f1094acd31a7a8e25db3007c9e06
Dmitry Matsiukhov <dimmarvel@mail.ru>
2023-05-14 14:42:14 +0300

    [BOOSTBOOK] add variant;

--- C++/Books/Boost application development in C++ Polukhin/variant.cpp
+++ C++/Books/Boost application development in C++ Polukhin/variant.cpp
@@ -4,9 +4,19 @@
 #include <string>
 #include <unordered_map>
 #include <variant>
+#include <memory>
+
+struct type_t
+{
+    std::string a;
+    std::string b;
+
+}typedef type_t;
+
 
 typedef boost::variant<int, const char*, std::string> var_t;
 typedef std::variant<int, std::string> var2_t;
+typedef std::variant<std::shared_ptr<type_t>, std::string> var3_t;
 
 void example1()
 {
@@ -46,10 +56,41 @@ void example2()
     std::cout << "-----------------" << std::endl;
 }
 
+void example3()
+{
+    std::unordered_map<var3_t, int> mp;
+    std::shared_ptr<type_t> smth_val = std::make_shared<type_t>();
+    std::shared_ptr<type_t> smth_val2 = std::make_shared<type_t>();
+    smth_val->a = "qwe";
+    smth_val->b = "qwe";
+    smth_val2->a = "qwe2";
+    smth_val2->b = "qwe2";
+    std::string smth_val3 = "str";
+
+    //mp.insert(smth_val, 1);
+    mp.insert({smth_val, 1});
+    
+    for(const auto& it : mp)
+    {
+        if(std::holds_alternative<std::shared_ptr<type_t>>(it.first))
+        {
+            auto ptr = std::get<std::shared_ptr<type_t>>(it.first);
+            std::cout   << "its ptr type_t key - " << ptr->a << ":" << ptr->b
+                        << " - " << it.second << std::endl; 
+        }
+        else
+        {
+            std::cout << "its str key - " << it.second << std::endl;
+        }
+    }
+
+    std::cout << "-----------------" << std::endl;
+}
 
 int main(int argc, char* argv[])
 {
     example1();
     example2();
+    example3();
     return 0;
 }
