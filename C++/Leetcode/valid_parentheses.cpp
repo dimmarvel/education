c81a0dfdd8a0df64d492c8bf03af057e2b5283fd
Dmitry Matsiukhov <dimmarvel@mail.ru>
2025-03-23 22:31:05 +0300

    [LEETCODE] Valid Parentheses

---
+++ C++/Leetcode/valid_parentheses.cpp
@@ -0,0 +1,25 @@
+#include "p.hpp"
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c : s)
+        {
+            if(c == '[' || c == '{' || c == '(')
+                st.push(c);
+            else
+            {
+                if(st.empty()) return false;
+                char top = st.top();
+                if((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') ||
+                    (c == ']' && top == '['))
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        return st.empty();
+    }
+};
