34c245ba6bcc0ecef4ec0a09fa874fd923901a2c
Dmitry Matsiukhov <dimmarvel@mail.ru>
2025-03-23 22:24:07 +0300

    [LEETCODE] Add precompile

---
+++ C++/Leetcode/p.hpp
@@ -0,0 +1,11 @@
+#include <vector>
+#include <map>
+#include <cstdint>
+#include <unordered_set>
+#include <unordered_map>
+#include <list>
+#include <deque>
+#include <forward_list>
+#include <stack>
+
+using namespace std;
